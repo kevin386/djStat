ae3dec452de1105a38505ff38acb86aafd7b8dbe
kevin <null386@gmail.com>
2013-12-19 19:34:05 +0800

    完成宝贝管理/订单管理/流量统计/部分菜单管理模型

--- djStat/dbmgr/admin.py
+++ djStat/dbmgr/admin.py
@@ -14,9 +14,35 @@ class g168itemResourceAdmin(admin.ModelAdmin):
 	list_display = ('resid','specid','csize','count')
 	list_filter = ('resid','specid','csize')
 
+class g168OrderAdmin(admin.ModelAdmin):
+	list_display = ('orderid','cname','username','phone','address','remark','createdate','orderstatus')
+
+class menuTypeAdmin(admin.ModelAdmin):
+	list_display = ('type_name',)
+	fields = ('type_name',)
+
+class vistCountAdmin(admin.ModelAdmin):
+	readonly_fields = ('vdate','vnum')
+	def get_readmonly_fields(self, request, obj=None):
+		return self.readonly_fields
+
+class g168ExpressAdmin(admin.ModelAdmin):
+	fields = ('exname','excnname')
+
+class g168MenuItemAdmin(admin.ModelAdmin):
+	list_display = ('content','content_type','link_type','link_value','newline','menu_id','order_by','tagcontent','type_id')
 admin.site.register(g168_item,g168ItemAdmin)
 admin.site.register(g168_item_status)
 admin.site.register(g168_item_type)
 admin.site.register(g168_item_resource,g168itemResourceAdmin)
 admin.site.register(sys_color)
 admin.site.register(sys_size)
+admin.site.register(g168_order,g168OrderAdmin)
+admin.site.register(g168_express,g168ExpressAdmin)
+admin.site.register(g168_order_status)
+admin.site.register(visitorcounter,vistCountAdmin)
+admin.site.register(t_product_menu_type_approve,menuTypeAdmin)
+admin.site.register(t_product_menu_item_approve,g168MenuItemAdmin)
+admin.site.register(t_product_menu_approve)
+admin.site.register(g168_page_style)
+admin.site.register(g168_link_type)
--- djStat/dbmgr/models.py
+++ djStat/dbmgr/models.py
@@ -71,7 +71,8 @@ class g168_item(models.Model):
 	outward = models.TextField('整体外观')	
 	promo = models.TextField('参与活动')	
 	recommend = models.TextField('推荐理由')	
-	vistCount = models.DecimalField('访问量',max_digits=10,decimal_places=0)
+	#vistCount = models.DecimalField('访问量',max_digits=10,decimal_places=0)
+	vistCount = models.IntegerField('访问量')
 	feature = models.TextField('特色介绍')	
 	mainparameter = models.TextField('主要参数')	
 	fitting = models.TextField('配件描述')	
@@ -96,7 +97,8 @@ class g168_item_resource(models.Model):
 	specid = models.ForeignKey(sys_color,db_column='specid',verbose_name='颜色')
 	csize = models.ForeignKey(sys_size,db_column='csize',verbose_name='尺寸')
 	isdefault = models.DecimalField(max_digits=1,decimal_places=0)
-	count = models.DecimalField('库存量',max_digits=10,decimal_places=0)
+	#count = models.DecimalField('库存量',max_digits=10,decimal_places=0)
+	count = models.IntegerField('库存量')
 	cdesc = models.CharField(max_length=4096)
 	reserved1 = models.CharField(max_length=64)
 	reserved2 = models.CharField(max_length=128)
@@ -111,4 +113,183 @@ class g168_item_resource(models.Model):
 	def __unicode__(self):
 		return u'宝贝规格';
 
+class g168_express(models.Model):
+	exid = models.IntegerField('快递编号',primary_key=True,unique=True)
+	exname = models.CharField('英文名称',max_length=50)
+	excnname = models.CharField('中文名称',max_length=50)
+	class Meta:
+		db_table = 'g168_express'
+		verbose_name = '快递管理' 
+		verbose_name_plural = '快递管理'
+		app_label = u'订单管理'
 
+	def __unicode__(self):
+		return self.excnname;
+
+
+class g168_order_status(models.Model):
+	status = models.CharField('订单状态',max_length=250)
+	class Meta:
+		db_table = 'g168_order_status'
+		verbose_name = '订单状态' 
+		verbose_name_plural = '订单状态'
+		app_label = u'订单管理'
+
+	def __unicode__(self):
+		return self.status;
+
+
+class g168_order(models.Model):
+	orderid = models.CharField('订单编号',max_length=20)
+	productid = models.CharField('商品编号',max_length=10)
+	#cname = models.ForeignKey(g168_item,verbose_name='宝贝名称')
+	cname = models.CharField('宝贝名称', max_length=200)
+	#specid= models.ForeignKey(sys_color,verbose_name='颜色')
+	specid = models.DecimalField('颜色', max_digits=10, decimal_places=0)
+	#csize = models.ForeignKey(sys_size,verbose_name='尺寸')
+	csize = models.DecimalField('尺寸', max_digits=10, decimal_places=0)
+	#count = models.DecimalField('购买数量', max_digits=11,decimal_places=0)
+	count = models.IntegerField('购买数量')
+	username = models.CharField('联系人', max_length=20)
+	price = models.DecimalField('下单价格',max_digits=10,decimal_places=2)
+	phone = models.CharField('联系人号码', max_length=15)
+	address = models.CharField('地址',max_length=200)
+	remark = models.CharField('备注信息',max_length=250)
+	#managerid = models.DecimalField('管理员ID',max_digits=11,decimal_places=0)
+	managerid = models.IntegerField('管理员ID')
+	maremark = models.CharField('管理员备注', max_length=200)
+	createdate = models.DateTimeField('下单日期')
+	dealdate = models.DateTimeField('成交日期')
+	exnumber = models.CharField('快递单号',max_length=20)
+	#exid = models.DecimalField('快递公司编号',max_digits=11,decimal_places=0)
+	exid = models.IntegerField('快递公司编号')
+	exstatus = models.CharField('快递状态',max_length=1000)
+	#orderstatus = models.DecimalField('订单状态',max_digits=11,decimal_places=0)
+	#orderstatus = models.IntegerField('订单状态')
+	orderstatus = models.ForeignKey(g168_order_status,db_column='orderstatus', verbose_name='订单状态')
+	class Meta:
+		db_table = 'g168_order'
+		verbose_name = '订单' 
+		verbose_name_plural = '订单管理'
+		app_label = u'订单管理'
+
+	def __unicode__(self):
+		return self.orderid;
+
+
+class visitorcounter(models.Model):
+	vdate = models.DateField('日期')
+	vnum = models.DecimalField('访问量', max_digits=11,decimal_places=0)
+	class Meta:
+		db_table = 'visitorcounter'
+		verbose_name = '访问量' 
+		verbose_name_plural = '访问量统计'
+		app_label = u'访问量统计'
+
+	def __unicode__(self):
+		return '%s' % self.vdate; 
+
+class t_product_menu_type_approve(models.Model):
+	type_id = models.IntegerField(primary_key=True,unique=True)
+	type_name = models.CharField('类型',max_length=200)
+	add_date = models.DateTimeField('日期')
+	parent_id = models.DecimalField('父ID', max_digits=8, decimal_places=0)
+	class Meta:
+		db_table = 't_product_menu_type_approve'
+		verbose_name = '菜单类型' 
+		verbose_name_plural = '菜单类型'
+		app_label = u'菜单管理'
+
+	def __unicode__(self):
+		return '%s' % self.type_name; 
+
+class g168_page_style(models.Model):
+	id = models.IntegerField(primary_key=True,unique=True)
+	name = models.CharField(max_length=45,verbose_name='名称')
+	class Meta:
+		db_table = 'g168_page_style'
+		verbose_name = '模板类型' 
+		verbose_name_plural = '模板类型'
+		app_label = u'菜单管理'
+
+	def __unicode__(self):
+		return '%s' % self.name; 
+
+class g168_link_type(models.Model):
+	id = models.IntegerField(primary_key=True,unique=True)
+	name = models.CharField(max_length=45,verbose_name='名称')
+	class Meta:
+		db_table = 'g168_link_type'
+		verbose_name = '链接类型' 
+		verbose_name_plural = '链接类型'
+		app_label = u'菜单管理'
+
+	def __unicode__(self):
+		return '%s' % self.name; 
+
+
+class t_product_menu_approve(models.Model):
+	menu_id = models.IntegerField(primary_key=True,unique=True)
+	menu_name = models.CharField(max_length=200)
+	bg_color = models.CharField(max_length=200)
+	font_color = models.CharField(max_length=200)
+	link_color = models.CharField(max_length=200)
+	font_size = models.CharField(max_length=200)
+	type_id = models.DecimalField(max_digits=8,decimal_places=0)
+	add_date = models.DateTimeField()
+	is_index = models.DecimalField(max_digits=8,decimal_places=0)
+	rec_link = models.CharField(max_length=20)
+	rec_download = models.CharField(max_length=20)
+	user_id = models.DecimalField(max_digits=8,decimal_places=0)
+	is_special = models.DecimalField(max_digits=8,decimal_places=0)
+	status = models.DecimalField(max_digits=8,decimal_places=0)
+	reversed1 = models.CharField(max_length=16)
+	reversed2 = models.CharField(max_length=16)
+	reversed3 = models.CharField(max_length=32)
+	class Meta:
+		db_table = 't_product_menu_approve'
+		verbose_name = '菜单列表' 
+		verbose_name_plural = '菜单列表'
+		app_label = u'菜单管理'
+
+	def __unicode__(self):
+		return '%s' % self.menu_name; 
+	
+	
+class t_product_menu_item_approve(models.Model):
+	item_id = models.IntegerField(primary_key=True,unique=True)
+	content_type = models.ForeignKey(g168_page_style,db_column='content_type',verbose_name='模板类型')
+	content = models.CharField('内容',max_length=600)
+	#link_type = models.CharField('链接类型',max_length=32)
+	link_type = models.ForeignKey(g168_link_type,db_column='link_type',verbose_name='链接类型')
+	link_value = models.CharField('链接',max_length=400)
+	pay_type = models.DecimalField(max_digits=8,decimal_places=0)
+	newline = models.DecimalField('是否换行',max_digits=8, decimal_places=0)
+	bg_color = models.CharField(max_length=20)
+	font_color = models.CharField(max_length=20)
+	font_size = models.CharField(max_length=10)
+	align = models.CharField(max_length=10)
+	font_style = models.CharField(max_length=10)
+	menu_id = models.ForeignKey(t_product_menu_approve,db_column='menu_id',verbose_name='菜单')
+	order_by = models.DecimalField('排序',max_digits=8, decimal_places=0)
+	act_date = models.DateTimeField()
+	tagcontent = models.CharField('标签',max_length=32)
+	rankcycle = models.DecimalField(max_digits=8,decimal_places=0)
+	status = models.DecimalField(max_digits=8,decimal_places=0)
+	disptype = models.IntegerField()
+	isself = models.IntegerField()
+	type_id = models.ForeignKey(t_product_menu_type_approve,db_column='type_id', verbose_name='菜单类型')
+	reserved1 = models.IntegerField()
+	reserved2 = models.IntegerField()
+	reserved3 = models.IntegerField()
+	reserved4 = models.IntegerField()
+
+	class Meta:
+		db_table = 't_product_menu_item_approve'
+		verbose_name = '菜单内容' 
+		verbose_name_plural = '菜单内容'
+		app_label = u'菜单管理'
+		ordering = ['order_by']
+
+	def __unicode__(self):
+		return '%s' % self.content; 
