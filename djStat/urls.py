1874f6fcaab98c841b858ea4f8bf46ceaa446f47
kevin <null386@gmail.com>
2013-12-20 15:28:06 +0800

    当debug=False时，要运行collectstatic之后才能显示admin的style

--- djStat/urls.py
+++ djStat/urls.py
@@ -10,6 +10,9 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+	url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.MEDIA_ROOT},name='media'),
+	url(r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.STATIC_ROOT},name='static'),
 )
+
 urlpatterns += static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static.static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
