0bc6ef66c6f1397fb88989133558d200e415268e
kevin <null386@gmail.com>
2013-12-20 14:54:32 +0800

    配置static和media目录

--- djStat/settings.py
+++ djStat/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
@@ -86,4 +85,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+#用户上传的文件保存目录
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/').replace('\\','/')
+MEDIA_URL = "/media/"
+#css,js,Images保存目录
+STATIC_ROOT = os.path.join(BASE_DIR,'static/').replace('\\','/')
+STATIC_URL = "/static/"
+#把每个APP下的static目录添加到下面元组
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'djStat/dbmgr/static/').replace('\\','/'),)
+
+
--- djStat/urls.py
+++ djStat/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, include, url, static
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -10,3 +11,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+urlpatterns += static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static.static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
