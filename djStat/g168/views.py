e7a7c25cd046d4400a3449fc1585818ac3cd27cf
kevin <null386@gmail.com>
2014-01-02 19:17:11 +0800

    完成列表显示

--- djStat/g168/views.py
+++ djStat/g168/views.py
@@ -1,35 +1,58 @@
 #-*- coding:utf8 -*-
 from django.shortcuts import render_to_response
 from djStat.dbmgr.models import *
+from django.contrib.sites.models import get_current_site
 
 class Text():
-	def __init__(self, request=None, value="", href=""):
-		self.value = value
-		if request and not href == "" and not href.startswith("http://"):
-			href = "http://" + request.get_host() +"/" + href	
+	def __init__(self, base_url="", value="", href="", index=0):
+		try:
+			sindex = unicode(str(index)+".")
+			self.value = sindex + value
+		except:
+			self.value = value
+		#print "Text value=" + self.value
+		if base_url and not href == "" and not href.startswith("http://"):
+			href = base_url + href	
 		self.href = href
 
 class Image():
-	def __init__(self, request=None, alt="image",href=""):
+	def __init__(self, base_url, alt="image",href=""):
+		#print "Image href=" + href
 		self.alt = alt
-		if request and not href == "" and not href.startswith("http://"):
-			href = "http://" + request.get_host() +"/" + href	
+		if base_url and not href == "" and not href.startswith("http://"):
+			href = base_url + href	
 		self.href = href
 
 class Input():
-	def __init__(self, request=None, title="", name="", href=""):
+	def __init__(self, base_url="", title="", name="", href=""):
 		self.title = title
 		self.name = name
-		if request and not href == "" and not href.startswith("http://"):
-			href = "http://" + request.get_host() +"/" + href	
+		if base_url and not href == "" and not href.startswith("http://"):
+			href = base_url + href	
 		self.href = href
 
+class objPair():
+	def __init__(self, id="", name=""):
+		self.id = id
+		self.name = name
+
+class Spec():
+	def __init__(self, color=None, sizes=None):
+		self.color = color
+		self.sizes = sizes
+
 class Order():
-	def __init__(self, info=""):
-		pass
+	def __init__(self, href="", mprice="",cprice="",dprice="",days=0,specs=[]):
+		self.href = href
+		self.mprice = mprice
+		self.cprice = cprice
+		self.dprice = dprice
+		self.days = days
+		self.specs = specs
 
 class Sublist:
 	def __init__(self, style=0, text=None, image=None, order=None):
+		#print "Sublist style=%d" % style
 		self.style = style
 		self.text = text 
 		self.image = image
@@ -37,6 +60,7 @@ class Sublist:
 
 class MenuItem:
 	def __init__(self, type=0, obj=None, newline=0):
+		#print "MenuItem type=%d" % type
 		#类型
 		self.type = type 
 		#对象
@@ -44,42 +68,88 @@ class MenuItem:
 		#是否换行
 		self.newline = newline
 
-def proList(request,act="",tag="",type="",code="",price="",vist=""):
+def proList(request,act="",type="",orderby=""):
+	print ''.join(['http://', request.get_host(), request.get_full_path()])
+	base_url = "".join(['http://',request.get_host(),"/"])
 	if act == "ls":
-		if not price == "":
-			if price == "de":
-				if not type == "":
-					dbItems = g168_item.objects.filter(ctype=type).oder_by("-price")
-				else:
-					dbItems = g168_item.objects.oder_by("-price")
-			else:
-				if not type == "":
-					dbItems = g168_item.objects.filter(ctype=type).oder_by("price")
-				else:
-					dbItems = g168_item.objects.oder_by("price")
+		#按价格由高到低排序
+		if orderby == "-price":
+			print "oder by price -"
 			if not type == "":
-				dbItems = g168_item.objects.filter(ctype=type).oder_by("-price")
+				dbItems = g168_item.objects.filter(ctype=type).order_by("-dprice")
 			else:
-				dbItems = g168_item.objects.oder_by("-price")
-		elif not vist == "":
+				dbItems = g168_item.objects.order_by("-dprice")
+		#按价格由低到高排序
+		elif orderby == "+price":
+			print "oder by price +"
 			if not type == "":
-				dbItems = g168_item.objects.filter(ctype=type).oder_by("vistCount")
+				dbItems = g168_item.objects.filter(ctype=type).order_by("dprice")
 			else:
-				dbItems = g168_item.objects.oder_by("vistCount")
+				dbItems = g168_item.objects.order_by("dprice")
+		#按人气排序
+		elif orderby == "-vist":
+			print "oder by vist -"
+			if not type == "":
+				dbItems = g168_item.objects.filter(ctype=type).order_by("vistCount")
+			else:
+				dbItems = g168_item.objects.order_by("vistCount")
+		elif orderby == "+vist":
+			print "oder by vist +"
+			if not type == "":
+				dbItems = g168_item.objects.filter(ctype=type).order_by("vistCount")
+			else:
+				dbItems = g168_item.objects.order_by("vistCount")
+		#仅分类
 		elif not type == "":
-			dbItems = g168_item.objects.filter(ctype=type).oder_by("-price")
-	elif act == "schPro":
-		if not type == "":
-			dbItems = g168_item.objects.filter(ctype=type,cname__icontains=tag,itemcode__icontains=tag).oder_by("-price")
+			print "oder by type"
+			dbItems = g168_item.objects.filter(ctype=type).order_by("dprice")
 		else:
-			dbItems = g168_item.objects.filter(cname__icontains=tag,itemcode__icontains=tag).oder_by("-price")
-	elif act == "schOrd":
-		pass
-	for item in dbItems:
-		print item
+			print "oder by default"
+			dbItems = g168_item.objects.all()
+	elif act == "sch":
+		tag = request.POST.get("tag",None)
+		if tag:
+			dbItems = g168_item.objects.filter(cname__icontains=tag,itemcode__icontains=tag).order_by("-dprice")
+
+	menuItems = []
+	if dbItems:
+		sublists = []
+		i = 0
+		for item in dbItems:
+			i += 1
+			href=base_url+"%d" % item.id
+			mprice=item.mprice
+			cprice=item.cprice
+			dprice=item.dprice
+			ds = item.list_time-item.active_time
+			days=ds.days
+			dicSpecs = {}
+			dbSpecs = g168_item_resource.objects.filter(resid=item.id)
+			for sp in dbSpecs:
+				if sp.count > 0 and not sp.specid.id == '---':
+					color = objPair(sp.specid.id, sp.specid.name)
+					if not dicSpecs.has_key(sp.specid.id):
+						sizes = []
+						dicSpecs[sp.specid.id] = Spec(color, sizes)
+					if not sp.csize.name == '---':
+						size = objPair(sp.csize.id, sp.csize.name)
+						dicSpecs[sp.specid.id].sizes.append(size)
+			sublists.append(Sublist(
+				#产品列表点显示类型
+				style = 3,
+				text = Text(base_url=base_url, value=item.cname, href="#", index=i),
+				image = Image(base_url=base_url, href=item.preview_image),
+				order = Order(href=href,mprice=mprice,cprice=cprice,dprice=dprice,days=days,specs=dicSpecs.values())
+				))
+
+		menuitem = MenuItem(100,sublists)
+		menuItems.append(menuitem)
+	return render_to_response('prolist.wml', {'menuItems':menuItems})
 
 # Create your views here.
 def index(request,id="1"):
+	print ''.join(['http://', request.get_host(), request.get_full_path()])
+	base_url = "".join(['http://',request.get_host(),"/"])
 	dbMenuItems = t_product_menu_item_approve.objects.filter(menu_id=id).order_by('order_by')
 	menuItems = []
 	for menu in dbMenuItems:
@@ -95,22 +165,22 @@ def index(request,id="1"):
 				for item in items:
 					sublists.append(Sublist(
 						style = menu.disptype.id,
-						text = Text(request=request, value=item.cname, href="#"),
-						image = Image(request=request, href=item.preview_image),
+						text = Text(base_url=base_url, value=item.cname, href="#"),
+						image = Image(base_url=base_url, href=item.preview_image),
 						order = Order()
 						))
 			menuitem = MenuItem(menu.content_type.id,sublists,menu.newline)
 		#这是搜索框
 		elif menu.content_type.id == 5:
-			input = Input(request=request, title=menu.content, name="input"+str(menu.order_by), href="")
+			input = Input(base_url=base_url, title=menu.content, name="input"+str(menu.order_by), href="")
 			menuitem = MenuItem(menu.content_type.id,input,menu.newline)
 		#这是图片
 		elif menu.content_type.id == 1:
-			image = Image(request=request, href=menu.content)
+			image = Image(base_url=base_url, href=menu.content)
 			menuitem = MenuItem(menu.content_type.id,image,menu.newline)
 		#这是文本
 		elif menu.content_type.id == 0:
-			text = Text(request=request, value=menu.content, href=menu.link_value)
+			text = Text(base_url=base_url, value=menu.content, href=menu.link_value)
 			menuitem = MenuItem(menu.content_type.id,text,menu.newline)
 		else:
 			print "unknow content_type"
--- djStat/urls.py
+++ djStat/urls.py
@@ -5,12 +5,12 @@ from filebrowser.sites import site
 from django.contrib import admin
 admin.autodiscover()
 
-def proList(request,act="",tag="",type="",code="",price="",vist=""):
-	
 urlpatterns = patterns('',
-    url(r'^(?P<id>\d+)$', 'djStat.g168.views.index'),
-    #url(r'^$', 'djStat.g168.views.index'),
-    url(r'^act=(?P<id>.*)$', 'djStat.g168.views.proList'),
+    url(r'^$', 'djStat.g168.views.index'),
+    url(r'^(?P<id>\d+)/$', 'djStat.g168.views.index'),
+    url(r'^(?P<act>ls)/(?P<type>\w+)/$', 'djStat.g168.views.proList'),
+    url(r'^(?P<act>ls)/(?P<type>\d+)/(?P<orderby>[+-]price|vist)/$', 'djStat.g168.views.proList'),
+    url(r'^(?P<act>sch)/$', 'djStat.g168.views.proList'),
 
 	url(r'^grappelli/',include('grappelli.urls')),
 	url(r'^admin/filebrowser/', include(site.urls)),
